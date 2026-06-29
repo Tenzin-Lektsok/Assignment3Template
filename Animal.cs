523f910e5b987269fd1ddd5f9d7f7d608be7f1f8
Tenzin-Lektsok <tenzinlektsok7@gmail.com>
2026-06-29 00:10:12 -0400

    Define Animal class with properties for name and position
    Added properties for animal name, ID, and 3D position.

--- Animal.cs
+++ Animal.cs
@@ -1,1 +1,17 @@
 
+Using System; 
+public class Animal
+{
+   // store animal name and ID
+    public string Name { get; set; }
+    public string ID { get; set; }
+
+    //store animal position on 3D grid.
+    //z cooridinate is added as per instruction A3 to move birds move up and down.
+    public int X { get; set; }
+    public int Y { get; set; }
+    public int Z { get; set; }
+
+
+
+}
